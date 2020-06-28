f0e0de2ebbc3ee5d1beef876efb4ede47f84e0fa
leannedo <ngocdo0912@gmail.com>
2020-06-28 21:00:44 +0300

    add Burger component to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 // class component, used to manage state
 import React, { Component } from 'react';
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
     render() {
         return(
             <div>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build Controls</div>
             </div>
         )
