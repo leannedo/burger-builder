f8fd7b704f408a14dcf2b757aebbc24e0be6b0cf
leannedo <ngocdo0912@gmail.com>
2020-07-08 21:05:35 +0300

    add Burger js - css file

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+// functional component to render UI
+const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map( igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>;
+            });
+        }).reduce((arr, el) => {return arr.concat(el)}, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients !</p>;
+    }
+    //why don't we check it right at the beginning and divide into 2 cases
+
+    console.log(transformedIngredients);
+
+
+    return(
+        <div className={classes.Burger}>
+            <BurgerIngredient type={'bread-top'}/>
+            {transformedIngredients}
+            <BurgerIngredient type={'bread-bottom'}/>
+        </div>
+    );
+};
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,29 @@
 import React, { Component } from 'react';
 import Burger from "../../components/Burger/Burger";
 
+// class component to control state
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = props;
+    // }
+
+    state = {
+        ingredients: {
+            cheese : 0,
+            bacon : 0,
+            meat : 0,
+            salad : 0
+        }
+        // the 'ingredients' object will receive the value from the build controls
+        // when customers can select how many of each ingredient they want to add
+    }
+
+
     render() {
         return(
             <div>
-                <Burger/>
+                <Burger ingredients = {this.state.ingredients}/>
                 <div>Build Controls</div>
             </div>
         )
