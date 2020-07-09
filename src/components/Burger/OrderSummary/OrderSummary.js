633b6d2a8355e266c13bdda4bc27867522028ba8
leannedo <ngocdo0912@gmail.com>
2020-07-09 15:20:22 +0300

    Add cancel and continue button to order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from './../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const orderSummary = Object.keys(props.ingredients)
@@ -11,6 +12,10 @@ const orderSummary = (props) => {
             <ul>
                 {orderSummary}
             </ul>
+            <p><strong>Total Price: ${props.price.toFixed(2)} </strong></p>
+            <p>Continue to checkout ?</p>
+            <Button clicked={props.purchaseCancelled} btnType={"Danger"}>CANCEL</Button>
+            <Button clicked={props.purchaseContinued} btnType={"Success"}>CONTINUE</Button>
         </div>
     )
 }
