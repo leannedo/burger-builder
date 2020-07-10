922f1213c56927bb83763f7d548924ea1bad1298
leannedo <ngocdo0912@gmail.com>
2020-07-10 10:57:20 +0300

    Complete  navigation in toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from './Toolbar.module.css';
 import Logo from "../../Logo/Logo";
+import NavItems from './../NavItems/NavItems';
 
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <p>MENU</p>
         <Logo/>
         <nav>
-            ...
+            <NavItems/>
         </nav>
     </header>
 )
